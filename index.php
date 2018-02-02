c49aeb4dad19015dd043ed17381a5147b4fb239c
kikinbmk10 <kikinbmk10@gmail.com>
2018-02-02 13:38:36 -0600

    [ADD] la pagina home pleta con sus respectivos responsive, y con el header y footer listos, falta mofificar el nav bar

---
+++ index.php
@@ -0,0 +1,280 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <title>Dr. Arturo Flores</title>
+  <link rel="icon" type="image/png" href="assets/images/logos/isotipo.png">
+  <!-- [#TODO] Dont forget to update the FAVICON IMAGES in the next route -->
+  <!-- <link rel="icon" type="image/png" href="assets/images/favicon-180x180.png" sizes="128x128"> -->
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/normalize/4.2.0/normalize.min.css" integrity="sha256-K3Njjl2oe0gjRteXwX01fQD5fkk9JFFBdUHy/h38ggY=" crossorigin="anonymous" rel="stylesheet">
+  <link href="./assets/styles/main.css" rel="stylesheet">
+</head>
+
+<body>
+  <!--  Header  -->
+
+  <div class="header-container">
+    <header class="main-nav">
+      <a href="">
+        <img src="assets/images/logos/isotipo.png" class="logo-header" alt="">
+      </a>
+      <nav role="navigation">
+        <ul>
+          <a href="inicio">
+            <li class="active-nav">
+              INICIO
+            </li>
+          </a>
+          <a href="trayectoria">
+            <li>
+              TRAYECTORIA
+            </li>
+          </a>
+          <a href="">
+            <li>
+              SERVICIOS
+            </li>
+          </a>
+          <a href="">
+            <li>
+              ENFERMEDADES
+            </li>
+          </a>
+          <a href="">
+            <li>
+              CONTACTO
+            </li>
+          </a>
+        </ul>
+      </nav>
+      <div id="nav-icon">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </header>
+  </div>
+  <!--  Hero -->
+  <div class="hero-image">
+     <div class="hero-content">
+        <div class="hero-text">
+           <h2 class="hero-title-blue">YO ME ENCARGO</h2>
+           <h2 class="hero-title-red">DE SU CORAZÓN</h2>
+           <p class="hero-p">No se preocupe por nada, usted déjeme su corazón a mi.</p>
+           <button class="ripple">TESTIMONIOS</button>
+        </div>
+     </div>
+     <div class="hero-aviso">
+      <div class="aviso-content">
+        <div class="llamanosTxt"> 
+          <img src="assets/images/aviso/alert.svg" class="alertIcon" alt="">
+          <p>PARA EMERGENCIAS LLÁMENOS AL (33) 1242 7651</p>
+        </div>
+        <img src="assets/images/aviso/x.svg" class="closeIcon" alt="">
+      </div>
+    </div>
+     <!-- imagen corazon -->
+     <img src="assets/images/heart.png" class="img-heart" alt="">
+  </div>
+  <!--  Main content -->
+  <main class="j-workspace ">
+     <section class="j-wrap">
+        <!-- biografia -->
+         <div class="biografia-container">
+            <div class="biografia-content">
+               <div class="bio-title-container">
+                  <h2 class="bio-h2">BIOGRAFÍA</h2>
+                  <img class="bio-title-img" src="assets/images/logos/icono_heart.png" alt="">
+               </div>
+               <div class="bio-info">
+                  <p class="p-info-gray">Médico Cirujano por la Universidad Autónoma de Aguascalientes con Especialidad en Cardiología Intervencionista en arritmias cardiacas por el Instituto Mexicano del Seguro Social y la Universidad de Guadalajara.</p>
+                  <p>El Dr. Arturo FLores Anguiano es médico cirujano mexicano especialista en Cardiología/Electrofisiología Cardiaca. Originario de la ciudad de Aguascalientes, donde realizó sus estudios iniciales, trasladandose a la ciudad de Guadalajara, Jalisco en 2002, para completar su especialidad.</p>
+                  <p>Ha sido un pionero en las ciencias médicas del siglo XXI, con estudios en campo de cardiología y en las arritmias cardiacas. Innovador en la aplicación...</p>
+                  <button>VER MÁS +</button>
+               </div>
+            </div>
+            <div class="biografia-images">
+               <img src="assets/images/biografia/1.png" alt="">
+               <img src="assets/images/biografia/2.png" alt="">
+               <img src="assets/images/biografia/3.png" alt="">
+            </div>
+         </dvi>
+     </section>
+     <!-- slider Testimonios -->
+     <section class="testimonios">
+         <div class="testimonios-img">
+            <img src="assets/images/biografia/4.png" alt="">
+         </div>
+         <div class="testimonios-spaceWhite">
+            <div class="test-slider-content">
+               <div class="itemsSlider">
+                  <div class="sliderContent slides fade">
+                     <h2 class="p-hero1 caslon">TESTIMONIOS</h2>
+                     <h3 class="p-hero2">Trauma craneoencefálico</h3>
+                     <p class="p-hero2">Trató a mi hermana, ella tuvo un accidente en cuatrimoto y tuvo trauma craneoencefálico. Es un excelente neurocirujano, pese a lo grave que estuvo el accidente. Tiempo después necesitó una cirugía para quitar un higroma que se le formó, quedó sin secuelas. Le agradezco mucho doctor, Dios lo acompañe.</p>
+                     <div class="score">
+                        <div class="stars">
+                        <img src="assets/images/biografia/star.png" alt="">
+                        <img src="assets/images/biografia/star.png" alt="">
+                        <img src="assets/images/biografia/star.png" alt="">
+                        <img src="assets/images/biografia/star.png" alt="">
+                        <img src="assets/images/biografia/star.png" alt="">
+                        </div>
+                        <p class="persona">Real San José</p>
+                     </div>
+                  </div>
+                  <div class="sliderContent slides fade">
+                     <h2 class="p-hero1 caslon">TESTIMONIOS</h2>
+                     <h3 class="p-hero2">Epilepsia</h3>
+                     <p class="p-hero2">Vio a mi hijo de epilepsia, mi hijo tenia mas de 20 crisis por día. lo trataron muchos neurólogos y nadie lo controlo. Yo quería que fuera candidato a cirugía, el Dr analizó la posibilidad de operarlo, pero resulto que no era viable en su caso. le dio un tratamiento nuevo y mejoro muchisimo.</p>
+                     <div class="score">
+                        <div class="stars">
+                        <img src="assets/images/biografia/star.png" alt="">
+                        <img src="assets/images/biografia/star.png" alt="">
+                        <img src="assets/images/biografia/star.png" alt="">
+                        <img src="assets/images/biografia/star.png" alt="">
+                        <img src="assets/images/biografia/star.png" alt="">
+                        </div>
+                        <p class="persona">C.M. Puerta de Hierro</p>
+                     </div>
+                  </div>
+                  <div class="sliderContent slides fade">
+                     <h2 class="p-hero1 caslon">TESTIMONIOS</h2>
+                     <h3 class="p-hero2">Schwannoma Cerebral.</h3>
+                     <p class="p-hero2">En estos tiempos es difícil encontrar profesionistas honrados y que por encima de eso sean buenos médicos. El Dr. operó a mi esposo de un Schwannoma cerebral y quedó de maravilla. No se le nota ninguna marca en su cabeza y recuperó su audición. Los dolores de cabeza tambien desaparecieron. Lo mejor</p>
+                     <div class="score">
+                        <div class="stars">
+                           <img src="assets/images/biografia/star.png" alt="">
+                           <img src="assets/images/biografia/star.png" alt="">
+                           <img src="assets/images/biografia/star.png" alt="">
+                           <img src="assets/images/biografia/star.png" alt="">
+                           <img src="assets/images/biografia/star.png" alt="">
+                        </div>
+                        <p class="persona">Consultorio /Pablo Neruda</p>
+                     </div>
+                  </div>
+                  <span id="btn1" class="itemsDirections" onclick="plusIndex(-1)"><img src="assets/images/biografia/flecha2.png" alt=""></span>
+                  <span id="btn2" class="itemsDirections" onclick="plusIndex(+1)"><img src="assets/images/biografia/flecha.png" alt=""></span>
+               </div>
+            </div>
+         </div>
+     </section>
+     <!-- enfermedades -->
+     <section class="j-wrap">
+         <div class="home-enfermedades-container">
+            <div class="home-enfermedades-info">
+               <h2 class="enfermedades-h2">ENFERMEDADES</h2>
+               <p>Es un aparato electrónico, del tamaño de una moneda, que genera impulsos eléctricos que estimulan al corazón para generar un latido cardiaco.</p>
+               <p>¿Para qué sirve un marcapaso?</p>
+               <p>Para estimular el corazón, aumentando la frecuencia de su latidos o para evitar que deje de latir.</p>
+               <p>¿Cúanto esta indicado el marcapaso?</p>
+               <p>Se encuentras indicado en personas con desmayos (sincope), frecuencia cardiaca baja (bradicardia), o en arritmias cardiacas.</p>
+               <button>VER MÁS +</button>
+            </div>
+            <div class="home-enf-img">
+               <img src="assets/images/biografia/5.png" alt="">
+            </div>
+         </dvi>
+     </section>
+         
+  </main>
+  <footer>
+     <div class="ft-section-up">
+        <img src="assets/images/footer/Logotipo.png" alt="">
+        <div class="footer-list ft-l-1 list-1">
+           <h3>TRAYECTORIA</h3>
+           <a href="#">Biografía</a>
+           <a href="#">Labor</a>
+        </div>
+        <div class="footer-list list-2">
+           <h3>SERVICIOS</h3>
+           <a href="#">Consultas</a>
+           <a href="#">Enfermedades</a>
+           <a href="#">Dispositivos</a>
+        </div>
+        <div class="footer-list-2 list-3">
+           <h3>ENFERMEDADES</h3>
+           <a href="#">Maracapaso</a>
+           <a href="#">Cardidesfibrilador</a>
+           <a href="#">Resincronizador</a>
+           <a href="#">Estudio Electrorisiologico</a>
+           <a href="#">Arritmias</a>
+        </div>
+        <div class="footer-list list-4">
+           <h3>CASOS DE ÉXITO</h3>
+           <a href="#">Lorem Ipsum</a>
+           <a href="#">Lorem Ipsum</a>
+           <a href="#">Lorem Ipsum</a>
+           <a href="#">Lorem Ipsum</a>
+        </div>
+     </div>
+     <div class="ft-section-down">
+        <div class="ft-inner">
+           <div class="ft-title-content">
+              <p>CARDIÓLOGO&nbsp;&nbsp;|&nbsp;&nbsp;INTERNISTA&nbsp;&nbsp;|&nbsp;&nbsp;ARRITMIAS</p>
+              <div class="dgp-content">
+                 <p class="dgp-text">DGP&nbsp;&nbsp;</p>
+                 <p class="dgp-number">3442264</p>
+              </div>
+           </div>
+           <div class="ft-adress-content">
+              <div class="ft-adress-inner">
+                 <div class="adress">
+                    <p>San Juan Bosco (Beethoven) #4743-8 [Entre Pintores y Abogados]</p>
+                    <p>Col. Lomas de Guadalupe. Zapopan, Jal. C.P. 45038</p>
+                 </div>
+                 <div class="telefonos">
+                    <p><a href="tel:35636022">T. 3563 6022 </a>| <a href="tel:33425155">3342 5155</a></p>
+                    <div class="urgencias">
+                       <p class="p-urgencias">URGENCIAS&nbsp;&nbsp;&nbsp;&nbsp;</p>
+                       <p><a href="tel:3312427651">C. (33) 1242 7651</a></p>
+                    </div>
+                    <p><a href="mailto:arturofa98@hotmail.com">arturofa98@hotmail.com</a></p>
+                 </div>
+              </div>
+              <div class="redes">
+                 <a href=""><img src="assets/images/footer/facebook.png" alt=""></a>
+                 <a href=""><img src="assets/images/footer/instagram.png" alt=""></a>
+              </div>
+           </div>
+           <div class="ft-copyright-content">
+              <p>Copyright © Dr. Arturo Flores 2018. Todos los Derechos Reservados.</p>
+              <p><a href="#">Términos y condiciones / Políticas de privacidad</a></p>
+           </div>
+        </div>
+     </div>
+  </footer>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+  <script src="assets/scripts/ripple.js"></script>
+  <script src="assets/scripts/header.js"></script>
+  <script src="assets/scripts/closeAd.js"></script>
+  <script>
+      var slideIndex = 1;
+
+      showImage(slideIndex);
+
+      function plusIndex(n) {
+         showImage(slideIndex += n);
+      }
+
+      function showImage(n) {
+         var slide = document.getElementsByClassName("slides");
+
+         if (n > slide.length){
+            slideIndex = 1
+         };
+         if (n < 1){
+            slideIndex = slide.length
+         };
+         for (var i = 0; i < slide.length; i++) {
+            slide[i].style.display = "none";
+            
+         };
+         slide[slideIndex-1].style.display = "flex";
+      }
+ </script>
+</body>
+</html>
