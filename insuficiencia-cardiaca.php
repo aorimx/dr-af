aa6b6e4547079302958c30dae3e1b25451427ea4
kikinbmk10 <kikinbmk10@gmail.com>
2018-02-08 23:09:21 -0600

    [ADD] footer con los links y todas las vistas con AD

--- insuficiencia-cardiaca.php
+++ insuficiencia-cardiaca.php
@@ -64,6 +64,16 @@
       </div>
     </header>
   </div>
+  <!--  Aviso  -->
+  <div class="aviso-general">
+     <div class="aviso-content">
+        <div class="llamanosTxt"> 
+           <img src="assets/images/aviso/alert.svg" class="alertIcon" alt="">
+           <p>PARA EMERGENCIAS LLÁMENOS AL (33) 1242 7651</p>
+        </div>
+        <img src="assets/images/aviso/x.svg" class="closeIcon" alt="">
+     </div>
+  </div>
 
   <!--  Main content -->
   <main class="j-workspace ">
@@ -150,101 +160,12 @@
       </div>
     </section>
   </main>
-  <footer>
-     <div class="ft-section-up">
-        <img src="assets/images/footer/Logotipo.png" alt="">
-        <div class="footer-list ft-l-1 list-1">
-           <h3>TRAYECTORIA</h3>
-           <a href="#">Biografía</a>
-           <a href="#">Labor</a>
-        </div>
-        <div class="footer-list list-2">
-           <h3>SERVICIOS</h3>
-           <a href="#">Consultas</a>
-           <a href="#">Enfermedades</a>
-           <a href="#">Dispositivos</a>
-        </div>
-        <div class="footer-list-2 list-3">
-           <h3>ENFERMEDADES</h3>
-           <a href="#">Maracapaso</a>
-           <a href="#">Cardiodesfibrilador</a>
-           <a href="#">Resincronizador</a>
-           <a href="#">Estudio Electrorisiologico</a>
-           <a href="#">Arritmias</a>
-        </div>
-        <div class="footer-list list-4">
-           <h3>CASOS DE ÉXITO</h3>
-           <a href="#">Lorem Ipsum</a>
-           <a href="#">Lorem Ipsum</a>
-           <a href="#">Lorem Ipsum</a>
-           <a href="#">Lorem Ipsum</a>
-        </div>
-     </div>
-     <div class="ft-section-down">
-        <div class="ft-inner">
-           <div class="ft-title-content">
-              <p>CARDIÓLOGO&nbsp;&nbsp;|&nbsp;&nbsp;INTERNISTA&nbsp;&nbsp;|&nbsp;&nbsp;ARRITMIAS</p>
-              <div class="dgp-content">
-                 <p class="dgp-text">DGP&nbsp;&nbsp;</p>
-                 <p class="dgp-number">3442264</p>
-              </div>
-           </div>
-           <div class="ft-adress-content">
-              <div class="ft-adress-inner">
-                 <div class="adress">
-                    <p>San Juan Bosco (Beethoven) #4743-8 [Entre Pintores y Abogados]</p>
-                    <p>Col. Lomas de Guadalupe. Zapopan, Jal. C.P. 45038</p>
-                 </div>
-                 <div class="telefonos">
-                    <p><a href="tel:35636022">T. 3563 6022 </a>| <a href="tel:33425155">3342 5155</a></p>
-                    <div class="urgencias">
-                       <p class="p-urgencias">URGENCIAS&nbsp;&nbsp;&nbsp;&nbsp;</p>
-                       <p><a href="tel:3312427651">C. (33) 1242 7651</a></p>
-                    </div>
-                    <p><a href="mailto:arturofa98@hotmail.com">arturofa98@hotmail.com</a></p>
-                 </div>
-              </div>
-              <div class="redes">
-                 <a href=""><img src="assets/images/footer/facebook.png" alt=""></a>
-                 <a href=""><img src="assets/images/footer/instagram.png" alt=""></a>
-              </div>
-           </div>
-           <div class="ft-copyright-content">
-              <p>Copyright © Dr. Arturo Flores 2018. Todos los Derechos Reservados.</p>
-              <p><a href="#">Términos y condiciones / Políticas de privacidad</a></p>
-           </div>
-        </div>
-     </div>
-  </footer>
+  <?php require 'footer.php';?>
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
   <script src="assets/scripts/ripple.js"></script>
   <script src="assets/scripts/header.js"></script>
   <script src="assets/scripts/closeAd.js"></script>
-  <script>
-      var slideIndex = 1;
 
-      showImage(slideIndex);
-
-      function plusIndex(n) {
-         showImage(slideIndex += n);
-      }
-
-      function showImage(n) {
-         var slide = document.getElementsByClassName("slides");
-
-         if (n > slide.length){
-            slideIndex = 1
-         };
-         if (n < 1){
-            slideIndex = slide.length
-         };
-         for (var i = 0; i < slide.length; i++) {
-            slide[i].style.display = "none";
-            
-         };
-         slide[slideIndex-1].style.display = "flex";
-      }
- </script>
 </body>
 </html>
